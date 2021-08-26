18dda399b48188ad51456726fa9be1196428a1c4
Woutsthew <nekita351@gmail.com>
2021-08-26 20:24:23 +0400

    Добавьте файлы проекта.

---
+++ DirectoryReader/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryReader
+{
+    class Program
+    {
+        static List<objDirFile> ls;
+        static void Main(string[] args)
+        {
+            ls = new List<objDirFile>();
+            string path = Environment.CurrentDirectory;
+            string help = "you can used two command:\n1.dir - to select a directory\n2.search - run the program with the selected path";
+            showcurrentdir(path);
+            while(true)
+            {
+                string message = Console.ReadLine();
+                if (message.Contains("dir")) { path = message.Split(' ').Last(); showcurrentdir(path); }
+                else if (message.Contains("search")) SearchFolderAndFile(path);
+                else if (message.Contains("?") || message.Contains("help")) Console.WriteLine(help);
+                else Console.WriteLine($"\nunknown command\n{help}");
+            }
+        }
+
+        static private void showcurrentdir(string path)
+        {
+            string mes = $"explore directory: \n{path}";
+            Console.Clear(); Console.WriteLine(mes);
+        }
+        static private void SearchFolderAndFile(string path)
+        {
+            if (!Directory.Exists(path)) { Console.WriteLine("this path is no directory"); return; }
+
+            List<string> listdir = Directory.GetDirectories(path,"*", SearchOption.AllDirectories).ToList();
+            foreach (string line in listdir) ls.Add(new objDirFile(line, objDirFile.MimeType.Directory));
+
+            List<string> listfile = Directory.GetFiles(path,"*", SearchOption.AllDirectories).ToList();
+            foreach (string line in listfile) ls.Add(new objDirFile(line));
+
+            ls.ForEach(f => Console.WriteLine(f.Info()));
+        }
+    }
+}
---
+++ DirectoryReader/objDirFile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryReader
+{
+    class objDirFile
+    {
+        public string name { get; private set; }
+        public long size { get; private set; }
+        public MimeType mimeType { get; private set; }
+        #region ext
+        List<string> app = new List<string> { "exe", "lnk", "msi", "sln", "zip", "rar", "7z", "cmd" };
+        List<string> video = new List<string> { "mp4", "avi", "mkv", "mpeg", "webm" };
+        List<string> audio = new List<string> { "mp3" };
+        List<string> image = new List<string> { "png", "jpg", "bmp", "gif", "webp" };
+        List<string> text = new List<string> { "txt", "dll", "css", "csv", "html", "php", "json", "xml", "py" };
+        List<string> vendor = new List<string> { "pdf", "doc", "docx", "pptx", "ppt", "accdb", "pub", "xlsx", "xls", "odt", "odp", "ods" };
+        #endregion
+        public objDirFile(string Name)
+        {
+            name = Name;
+            size = new FileInfo(Name).Length;
+            string ext = Name.Split('.').Last();
+            if (app.Contains(ext)) mimeType = MimeType.Application;
+            else if (video.Contains(ext)) mimeType = MimeType.VideoFile;
+            else if (audio.Contains(ext)) mimeType = MimeType.AudioFile;
+            else if (image.Contains(ext)) mimeType = MimeType.ImageFile;
+            else if (text.Contains(ext)) mimeType = MimeType.TextFile;
+            else if (vendor.Contains(ext)) mimeType = MimeType.VendorFile;
+            else mimeType = MimeType.NonStandartFile;
+        }
+        public objDirFile(string Name, MimeType MimeType)
+        {
+            name = Name;
+            size = new DirectoryInfo(Name).GetFiles("*", SearchOption.AllDirectories).Sum(fl => fl.Length);
+            mimeType = MimeType;
+        }
+
+        public string Info() { return $"{name} {size} {mimeType}"; }
+        public enum MimeType
+        {
+            Directory,
+            Application,
+            VideoFile,
+            AudioFile,
+            ImageFile,
+            TextFile,
+            VendorFile,
+            NonStandartFile
+        }
+    }
+}
